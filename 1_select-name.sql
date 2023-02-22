431d56fd43c55aabca9d0c4e8cb6578607f8313b
kristina-sparrow <118679841+kristina-sparrow@users.noreply.github.com>
2023-02-22 16:08:32 -0700

    Set up files

---
+++ 1_select-name.sql
@@ -0,0 +1,29 @@
+-- 1. Find the country that start with Y.
+
+-- 2. Find the countries that end with y.
+
+-- 3. Find the countries that contain the letter x.
+
+-- 4. Find the countries that end with land.
+
+-- 5. Find the countries that start with C and end with ia.
+
+-- 6. Find the country that has oo in the name.
+
+-- 7. Find the countries that have three or more a in the name.
+
+-- 8. Find the countries that have "t" as the second character.
+
+-- 9. Find the countries that have two "o" characters separated by two others.
+
+-- 10. Find the countries that have exactly four characters.
+
+-- 11. Find the country where the name is the capital city.
+
+-- 12. Find the country where the capital is the country plus "City".
+
+-- 13. Find the capital and the name where the capital includes the name of the country.
+
+-- 14. Find the capital and the name where the capital is an extension of name of the country.
+
+-- 15. Show the name and the extension where the capital is an extension of name of the country.
---
+++ 2_select-from-world.sql
@@ -0,0 +1,25 @@
+-- 1. Observe the result of running this SQL command to show the name, continent and population of all countries.
+
+-- 2. Show the name for the countries that have a population of at least 200 million.
+
+-- 3. Give the name and the per capita GDP for those countries with a population of at least 200 million.
+
+-- 4. Show the name and population in millions for the countries of the continent 'South America'. Divide the population by 1000000 to get population in millions.
+
+-- 5. Show the name and population for France, Germany, Italy.
+
+-- 6. Show the countries which have a name that includes the word 'United'.
+
+-- 7. Show the countries that are big by area or big by population. Show name, population and area.
+
+-- 8. Show the countries that are big by area (more than 3 million) or big by population (more than 250 million) but not both. Show name, population and area.
+
+-- 9. Show the name and population in millions and the GDP in billions for the countries of the continent 'South America'. Use the ROUND function to show the values to two decimal places.
+
+-- 10. Show the name and per-capita GDP for those countries with a GDP of at least one trillion (1000000000000; that is 12 zeros). Round this value to the nearest 1000.
+
+-- 11. Show the name and capital where the name and the capital have the same number of characters.
+
+-- 12. Show the name and the capital where the first letters of each match. Don't include countries where the name and the capital are the same word.
+
+-- 13. Find the country that has all the vowels and no spaces in its name.
